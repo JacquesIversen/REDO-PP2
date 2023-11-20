642a8203e13fb5c6cc29566dc1ddb7d8e5bbffdf
JacquesIversen <jackiracki1@gmail.com>
2023-11-20 18:39:02 +0000

    questions added in script

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const quizzes = [
+  {
+    name: "Alaska Geography",
+    questions: [
+      {
+        image: "alaska-image.jpg",
+        question: "What is the largest city in Alaska?",
+        options: ["Anchorage", "Fairbanks", "Juneau"],
+        correctAnswer: "Anchorage",
+      },
+      {
+        image: "alaska-image.jpg",
+        question: "ff",
+        options: ["Anchorage", "Fairbanks", "Juneau"],
+        correctAnswer: "Anchorage",
+      },
+      {
+        image: "alaska-image.jpg",
+        question: "dd",
+        options: ["Anchorage", "Fairbanks", "Juneau"],
+        correctAnswer: "Anchorage",
+      },
+      {
+        image: "alaska-image.jpg",
+        question: "ss",
+        options: ["Anchorage", "Fairbanks", "Juneau"],
+        correctAnswer: "Anchorage",
+      },
+      {
+        image: "alaska-image.jpg",
+        question: "bb",
+        options: ["Anchorage", "Fairbanks", "Juneau"],
+        correctAnswer: "Anchorage",
+      },
+    ],
+  },
+];
+
+
+
+
+displayQuiz();
