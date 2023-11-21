bbcf687524c480768853eb512fd1a27a33728f1c
JacquesIversen <jackiracki1@gmail.com>
2023-11-21 09:13:01 +0000

    added dummy scores to localstorage

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,5 +1,16 @@
 const highscores = JSON.parse(localStorage.getItem("highscores")) || [];
 
+/* Dummy Highscore: */
+
+const dummyHighscores = [
+  { username: "John Woke", score: 6 },
+  { username: "Jimmi Sleep", score: 5 },
+  { username: "That Doe Girl", score: 8 },
+];
+
+localStorage.setItem("highscores", JSON.stringify(dummyHighscores));
+highscores.push(...dummyHighscores);
+
 const highscoreList = document.getElementById("highscore-list");
 
 highscores.forEach((highscore) => {
