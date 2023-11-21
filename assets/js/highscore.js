d27be6994bc9776bee4e9f00cab24846536306e8
JacquesIversen <jackiracki1@gmail.com>
2023-11-21 10:09:29 +0000

    highscore added dummy data

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,15 +1,18 @@
 const highscores = JSON.parse(localStorage.getItem("highscores")) || [];
 
-/* Dummy Highscore: */
-
 const dummyHighscores = [
   { username: "John Woke", score: 6 },
   { username: "Jimmi Sleep", score: 5 },
   { username: "That Doe Girl", score: 8 },
 ];
 
-localStorage.setItem("highscores", JSON.stringify(dummyHighscores));
-highscores.push(...dummyHighscores);
+const storedHighscores = JSON.parse(localStorage.getItem("highscores"));
+
+if (!storedHighscores || storedHighscores.length === 0) {
+  localStorage.setItem("highscores", JSON.stringify(dummyHighscores));
+  highscores.push(...dummyHighscores);
+}
+
 highscores.sort((a, b) => b.score - a.score);
 
 const highscoreList = document.getElementById("highscore-list");
@@ -17,5 +20,6 @@ const highscoreList = document.getElementById("highscore-list");
 highscores.forEach((highscore) => {
   const listItem = document.createElement("li");
   listItem.textContent = `${highscore.username} - ${highscore.score}`;
+  listItem.classList.add("highscore-li");
   highscoreList.appendChild(listItem);
 });
